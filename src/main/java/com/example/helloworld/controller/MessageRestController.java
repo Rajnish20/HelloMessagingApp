49deee762fe19b228036c1b2762c12ba96102b99
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-07-20 16:24:17 +0530

    Add-display hello message with name and extract name from query params

--- src/main/java/com/example/helloworld/controller/MessageRestController.java
+++ src/main/java/com/example/helloworld/controller/MessageRestController.java
@@ -2,6 +2,7 @@ package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class MessageRestController {
     @GetMapping("/hello")
     public String helloMessage(){
         return "Hello From BridgeLabs";
+    }
+
+    @GetMapping("/helloName")
+    public String sayHelloMark(@RequestParam String name){
+        return "Hello " + name;
     }
 }
